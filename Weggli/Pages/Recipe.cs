142872758d0d77ebca183837d88031fc9e25e48f
Simon Wälchli <walcs2@bfh.ch>
2022-12-04 18:23:38 +0100

    initial branch commit

---
+++ Weggli/Pages/Recipe.cs
@@ -0,0 +1,13 @@
+﻿namespace Weggli.Pages;
+
+public class Recipe
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+
+    public Recipe(string name, string description)
+    {
+        Name = name;
+        Description = description;
+    }
+}
